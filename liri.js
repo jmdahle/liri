fc4b7e02818cd06a8a5b16827a23c9e4a082641c
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-06-26 19:54:43 -0500

    formatted output; working song, concert, movie

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ switch (cmd) {
         // everything after the cmd is the artist name
         var artistName = process.argv.slice(3).join(' ');
         // constructor for events returned from API
-        function ArtistEvent (name, loc, date) {
+        function ArtistEvent(name, loc, date) {
             this.venueName = name;
             this.venueLocation = loc;
             this.eventDate = date;
@@ -22,7 +22,7 @@ switch (cmd) {
         var url = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=c`
         // console.log(url);
         axios.get(url)
-            .then( (response) => {
+            .then((response) => {
                 // console.log(JSON.stringify(response.data, null, 2) );
                 // set a variable equal to the events so we can loop through them
                 var returnedEvents = response.data;
@@ -31,91 +31,145 @@ switch (cmd) {
                 for (let i = 0; i < returnedEvents.length; i++) {
                     var eName = returnedEvents[i].venue.name;
                     var eLoc = returnedEvents[i].venue.city;
-                    eLoc += ', ' + returnedEvents[i].venue.region;
-                    eLoc += ' ' + returnedEvents[i].venue.country;
-                    var eDate = returnedEvents[i].datetime;
-                    var thisEvent = new ArtistEvent (eName, eLoc, eDate);
+                    if (returnedEvents[i].venue.region === '') {
+                        eLoc += ', ' + returnedEvents[i].venue.country;
+                    } else {
+                        eLoc += ', ' + returnedEvents[i].venue.region;
+                        eLoc += ' ' + returnedEvents[i].venue.country;
+                    }
+                    var eDate = moment(returnedEvents[i].datetime, moment.ISO_8601).format('MM/DD/YYYY');
+                    var thisEvent = new ArtistEvent(eName, eLoc, eDate);
                     // console.log(eName, eLoc, eDate);
                     concertEvents.push(thisEvent);
                 }
                 // output the events
                 console.log(`Upcoming concerts for ${artistName}:`);
-                console.log (
-                    textPad('Date',20,' '),
-                    textPad('Venue',40,' '),
-                    textPad('Location',40,' ')
+                // headings
+                console.log(
+                    textPad('Date', 10, ' '),
+                    textPad('Venue', 34, ' '),
+                    textPad('Location', 34, ' ')
                 );
-                console.log (
-                    textPad('',20,'='),
-                    textPad('',40,'='),
-                    textPad('',40,'=')
+                // headering 'underline'
+                console.log(
+                    textPad('', 10, '='),
+                    textPad('', 34, '='),
+                    textPad('', 34, '=')
                 );
+                // data
                 for (let j = 0; j < concertEvents.length; j++) {
                     console.log(
-                        textPad(concertEvents[j].eventDate,20,' '), 
-                        textPad(concertEvents[j].venueName,40,' '), 
-                        textPad(concertEvents[j].venueLocation,40,' ')
-                        );
+                        textPad(concertEvents[j].eventDate, 10, ' '),
+                        textPad(concertEvents[j].venueName, 34, ' '),
+                        textPad(concertEvents[j].venueLocation, 34, ' ')
+                    );
                 }
             })
-            .catch( (error) => {
+            .catch((error) => {
                 console.log(error);
             });
-            
+
         break;
     case 'spotify-this-song':
-            console.log(`accepted command ${cmd}`);
-            // everything after the cmd is the song name
-            var songName = process.argv.slice(3).join(' ');
-            // use node-spotify-api 
-            var spotify = new Spotify(keys.spotify);
-            spotify.search({ type: 'track', query: songName, limit: 1 }, (err, data) => {
-                if (err) {
-                  return console.log('Error occurred: ' + err);
-                }
-            //   console.log( JSON.stringify(data.tracks.items, null, 2) ); 
-              var songInfo = data.tracks.items[0];
-              var artistName = songInfo.artists[0].name;
-              var albumName = songInfo.album.name;
-              var songUrl = songInfo.external_urls.spotify;
-              console.log(songName, artistName, albumName, songUrl);
-              });
-            break;
+        console.log(`accepted command ${cmd}`);
+        // everything after the cmd is the song name
+        var songName = process.argv.slice(3).join(' ');
+        // use node-spotify-api 
+        var spotify = new Spotify(keys.spotify);
+        spotify.search({ type: 'track', query: songName, limit: 1 }, (err, data) => {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } else {
+                //   console.log( JSON.stringify(data.tracks.items, null, 2) ); 
+                // get only the first song returned
+                var songInfo = data.tracks.items[0];
+                var artistName = songInfo.artists[0].name;
+                var albumName = songInfo.album.name;
+                var songUrl = songInfo.external_urls.spotify;
+                // output the song information
+                // console.log(songName, artistName, albumName, songUrl);
+                // console.log(`Song: ${songName}\nArtist: ${artistName}\nAlbum: ${albumName}\nSpotify URL: ${songUrl}`);
+                console.log (
+                    textPad('Song:', 15, ' '),
+                    textPad(songName, 60, ' ')
+                );
+                console.log (
+                    textPad('Artist:', 15, ' '),
+                    textPad(artistName, 60, ' ')
+                );
+                console.log (
+                    textPad('Album:', 15, ' '),
+                    textPad(albumName, 60, ' ')
+                );
+                console.log(
+                    textPad('Spotify URL:', 15, ' '),
+                    textPad(songUrl, 60, ' ')
+                );
+            }
+        });
+        break;
     case 'movie-this':
-            console.log(`accepted command ${cmd}`);
-            // everything after the cmd is the movie title
-            var movieTitle = process.argv.slice(3).join(' ');
-            // use axios
-            var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
-            axios.get(queryUrl)
-              .then( (response) => {
-                console.log(JSON.stringify(response.data,null,2));
+        console.log(`accepted command ${cmd}`);
+        // everything after the cmd is the movie title
+        var movieTitle = process.argv.slice(3).join(' ');
+        // use axios
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl)
+            .then((response) => {
+                // console.log(JSON.stringify(response.data,null,2));
                 movieData = response.data;
                 var movieYear = movieData.Year;
                 var imdbRating = movieData.imdbRating;
                 //get rotten tomatoes score
                 var rtRating = 'no rating'
                 for (let i = 0; i < movieData.Ratings.length; i++) {
-                    if (movieData.Ratings.Source === 'Rotten Tomatoes') {
-                        rtRating = movieData.Ratings.Value;
+                    if (movieData.Ratings[i].Source === 'Rotten Tomatoes') {
+                        rtRating = movieData.Ratings[i].Value;
                     }
                 }
-              })
-              .catch( (error) => {
+                var movieCountry = movieData.Country;
+                var moviePlot = movieData.Plot;
+                var movieActors = movieData.Actors;
+                // output movie info
+                // console.log(`${movieTitle}\nYear: ${movieYear}\nCountry: ${movieCountry}\nRatings: IMDB-${imdbRating}, Rotten Tomatoes-${rtRating}\nActors: ${movieActors}\nPlot: ${moviePlot}`);
+                console.log (
+                    movieTitle
+                );
+                console.log(
+                    moviePlot
+                );
+                console.log(
+                    textPad('Actors', 15, ' '),
+                    textPad(movieActors, 60, ' ')
+                );
+                console.log(
+                    textPad('Year:', 15, ' '),
+                    textPad(movieYear, 60, ' ' )
+                );
+                console.log(
+                    textPad('Country:', 15, ' '),
+                    textPad(movieCountry, 60, ' ')
+                );
+                console.log(
+                    textPad('Ratings:', 15, ' '),
+                    textPad(`IMDB- ${imdbRating}, Rotten Tomatoes- ${rtRating}`, 60, ' ')
+                );
+            })
+            .catch((error) => {
                 console.log(error);
-              });
-            break;
+            });
+        break;
     case 'do-what-it-says':
-            console.log(`accepted command ${cmd}`);
-            break;
-    default: 
+        console.log(`accepted command ${cmd}`);
+        break;
+    default:
         console.log(`bad command ${cmd}`);
-        break;        
+        break;
 }
 
-function textPad (string, len, padchar) {
+function textPad(string, len, padchar) {
     var emptyString = padchar.repeat(len);
     var newString = string + emptyString;
-    newString = newString.substring(0,len-1);
+    newString = newString.substring(0, len);
     return newString;
 }
